55785b84fb44e1af967c7b90aab5599a5c0de856
Rodolfo Lima <rodolfo-m16@hotmail.com>
2025-01-23 20:42:11 -0300

    #23 - Find by id news

--- src/services/news.service.js
+++ src/services/news.service.js
@@ -2,5 +2,10 @@ import News from '../models/News.js'
 
 export const createService = (body) => News.create(body)
 
-export const findAllService = () => News.find()
+export const findAllService = (offset, limit) => News.find().sort({_id: -1}).skip(offset).limit(limit).populate("user")
 
+export const countNews = () => News.countDocuments()
+
+export const topNewsService = () => News.findOne().sort({ _id: -1 }).populate("user");
+
+export const findByIdService = (id) => News.findById(id).populate("user");
